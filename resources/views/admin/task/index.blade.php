e4099d868071e6e12ab7be68d7831b93dd85f2e5
ScriptSinger <ufamasters102@gmail.com>
2023-04-17 22:43:51 +0500

    first commit

---
+++ resources/views/admin/task/index.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::page', ['iFrameEnabled' => true])
+
+@section('title', 'Задания')
+
+@section('content_header')
+    <h1>Задания</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>
+        console.log('Hi!');
+    </script>
+@stop
