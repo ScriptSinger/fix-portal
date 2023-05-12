3f7bd67a4c0ec550f9b82db6c4eeee707e2ec7f4
ScriptSinger <ufamasters102@gmail.com>
2023-05-12 15:37:25 +0500

    model relationships

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    public function consumers()
+    {
+        return $this->hasMany(Consumer::class);
+    }
 }
